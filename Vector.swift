5b013263a63c7c84f4c97ece4632b0e653fa42ca
atjokine <attejokinen98@gmail.com>
2021-10-28 23:03:20 +0300

    Added documentation

--- Vector.swift
+++ Vector.swift
@@ -11,27 +11,33 @@ class Vector {
     public var x: Double
     public var y: Double
     
-    init(x: Double, y: Double)
+    init(_ x: Double, _ y: Double)
     {
         self.x = x
         self.y = y
     }
     
     
-    //Calculates distance between two vector using pythagorean theorem
-    func Distance(vector1: Vector, vector2: Vector) -> Double {
+    /// Calculates distance between two vector using pythagorean theorem. By default rounds to two decimals
+    ///  - Parameters:
+    ///    - vector1: First vector
+    ///    - vector2: Second Vector
+    ///    - roundTo: How accurately the result is rounded. Default is 100
+    static func Distance(_ vector1: Vector, _ vector2: Vector, _ roundTo: Double = 100) -> Double {
         let xDistance: Double = abs(vector2.x - vector1.x)  // Absolute value of distance in x-axis
         let yDistance: Double = abs(vector1.y - vector2.y)  // Absolute value of distance in y-axis
         
-        return (xDistance*xDistance + yDistance*yDistance).squareRoot()  // Square root of sides squared
+        let result: Double = ((xDistance*xDistance) + (yDistance*yDistance)).squareRoot()  // Square root of sides squared
+        
+        return round(roundTo * result) / roundTo // rounds to .01
     }
     
     
-    // Calculates a third vector between two vectors
-    func Average(vector1: Vector, vector2: Vector) -> Vector {
+    /// Calculates a third vector between two vectors
+    func Average(_ vector1: Vector, _ vector2: Vector) -> Vector {
         let x3: Double = (vector1.x + vector2.x) / 2
         let y3: Double = (vector1.y + vector2.y) / 2
         
-        return Vector(x: x3, y: y3)
+        return Vector(x3, y3)
     }
 }
